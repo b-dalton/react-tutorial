040f7d390a485ab02766482151f53db3e142d426
Ben <ben.dalton@madetech.com>
2020-11-15 19:46:23 +0000

    create handleAddToDo function to update todos state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import ToDoList from './ToDoList';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [todos, setToDos] = useState([]);
+  const todoNameRef = useRef();
+
+  function handleAddTodo(event) {
+    const name = todoNameRef.current.value
+    if (name === '') return
+    setToDos(prevTodos => {
+      return [...prevTodos, {id: uuidv4(), name: name, complete: false}]
+    })
+    todoNameRef.current.value = null
+  }
+
   return (
     <div>
       <ToDoList todos={todos}/>
-      <input type="text" />
-      <button>Add Todo</button>
+      <input ref={todoNameRef} type="text" />
+      <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear completed</button>
       <div>0 left to do</div>
     </div>
