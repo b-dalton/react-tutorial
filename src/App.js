ecf215d0f37c12c0822f0741a3ab93be1d189aa7
Ben <ben.dalton@madetech.com>
2020-11-15 19:52:46 +0000

    create useEffect with function to store todo items in local storage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import ToDoList from './ToDoList';
 import { v4 as uuidv4 } from 'uuid';
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
   const [todos, setToDos] = useState([]);
   const todoNameRef = useRef();
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
 
   function handleAddTodo(event) {
     const name = todoNameRef.current.value
